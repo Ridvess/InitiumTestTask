96133f9ad370b38813f406edafc53e1fa3f0c8cd
Roman_Filipenko <Roman_Filipenko@epam.com>
2022-04-18 20:37:10 +0300

    first draft

---
+++ src/app/Components/row-data/row-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-row-data',
+  templateUrl: './row-data.component.html',
+  styleUrls: ['./row-data.component.scss']
+})
+export class RowDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './Components/table/table.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { RowDataComponent } from './Components/row-data/row-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    HeaderComponent,
+    RowDataComponent
   ],
   imports: [
     BrowserModule
